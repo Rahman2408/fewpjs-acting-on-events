1a44fdaaa77e183385f3b6d29317e9e9ef0fe593
Rahman Elsirafy <rahman2408@gmail.com>
2021-08-31 08:22:01 -0400

    commit tree 9168bfbe14247dc1acb9bbc7ef92aee1e47b9cbd

--- index.js
+++ index.js
@@ -1,1 +1,54 @@
 // Your code here
+const board = document.getElementById("game");
+let dodger = document.getElementById("dodger");
+
+
+
+function moveDodgerLeft() {
+  let leftNumbers = dodger.style.left.replace('px', '');
+  let left = parseInt(leftNumbers);
+if (left > 0) {
+  dodger.style.left = `${left - 10}px`;
+}
+}
+
+function moveDodgerRight() {
+  let leftNumbers = dodger.style.left.replace('px', '');
+  let left = parseInt(leftNumbers);
+if (left < 760) {
+  dodger.style.left = `${left + 10}px`;
+}
+}
+
+function moveDodgerDown() {
+  let bottomNumbers = dodger.style.bottom.replace('px', '');
+  let bottom = parseInt(bottomNumbers);
+if (bottom > 0) {
+  dodger.style.bottom = `${bottom - 10}px`;
+}
+}
+
+function moveDodgerUp() {
+  let bottomNumbers = dodger.style.bottom.replace('px', '');
+  let bottom = parseInt(bottomNumbers);
+if (bottom < 780) {
+  dodger.style.bottom = `${bottom + 10}px`;
+}
+}
+
+document.addEventListener('keydown', function(e) {
+  switch(e.key) {
+    case 'ArrowLeft':
+      moveDodgerLeft()
+      break;
+    case 'ArrowRight':
+      moveDodgerRight()
+      break;
+    case 'ArrowUp':
+      moveDodgerUp()
+      break;
+    case 'ArrowDown':
+      moveDodgerDown()
+      break;
+  }
+});
